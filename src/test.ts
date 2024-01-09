755fb57522500c461740e0aa7421724cad05abcc
Vignesh Nagamani <vigneshnagamani@yahoo.in>
2024-01-09 15:19:19 +0530

    dist

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,7 @@
 // src/index.js
 import express from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import * as DotEnv from 'dotenv';
+DotEnv.config();
 
 const app = express();
 const port = process.env.PORT;
@@ -10,7 +9,7 @@ const port = process.env.PORT;
 app.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
 });
-
+console.log('process.env',process.env);
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
